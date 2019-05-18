ea397d87315b6cb9065176723025e041c8433f70
RILEYSXPS\spill <rcspille@asu.edu>
2019-05-18 01:47:23 -0700

    Test to see if sticky scroll works.

--- sticky-scroll.js
+++ sticky-scroll.js
@@ -3,13 +3,21 @@ $(window).scroll(function() {
     var currentScroll = $(window).scrollTop();
     if (currentScroll >= fixmeTop) {
         $('#call_panel').css({
-            position: 'fixed',
-            top: '200',
-            left: '0'
+          #call_panel{
+            width: 100%;
+            padding: 20px 0;
+            position: fixed;
+            top: 100;
+          }
         });
     } else {
         $('#call_panel').css({
-            position: 'static'
+          #call_panel{
+            width: 100%;
+            padding: 20px 0;
+            position: absolute;
+            bottom: 0;
+          }
         });
     }
 });
