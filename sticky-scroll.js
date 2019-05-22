2df17ce47026dd24fa665bcebcac0abad791bca6
RILEYSXPS\spill <rcspille@asu.edu>
2019-05-22 14:42:08 -0700

    Moved sticky-scroll js to separate file.

--- sticky-scroll.js
+++ sticky-scroll.js
@@ -1,23 +1,24 @@
-var fixmeTop = $('#call_panel').offset().top;
-$(window).scroll(function() {
-    var currentScroll = $(window).scrollTop();
-    if (currentScroll >= fixmeTop) {
-        $('#call_panel').css({
-          #call_panel{
-            width: 100%;
-            padding: 20px 0;
-            position: fixed;
-            top: 100;
-          }
+// STICKY SCROLL CLASS
+if($(window).width() >= 400){
+    (function($) {
+        var element = $('.to_move_content'),
+            header = $('#top_nav'),
+            originalY = (element.offset().top) - 76;
+
+        // Space between element and top of screen (when scrolling)
+        var topMargin = 0;
+
+        // Should probably be set in CSS; but here just for emphasis
+        element.addClass('sticky');
+
+        $(window).on('scroll', function(event) {
+            var scrollTop = $(window).scrollTop();
+
+            element.stop(false, false).animate({
+                top: scrollTop < originalY
+                    ? 0
+                    : scrollTop - originalY + topMargin
+            }, 0);
         });
-    } else {
-        $('#call_panel').css({
-          #call_panel{
-            width: 100%;
-            padding: 20px 0;
-            position: absolute;
-            bottom: 0;
-          }
-        });
-    }
-});
+    })(jQuery);
+}
